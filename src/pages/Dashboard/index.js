662a8dfb06346aff24907fafbc88ac76a5b4d5c5
marcelomac <marcelo.a.mac@gmail.com>
2020-01-07 10:36:46 -0300

    20200107

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
-
+import api from '~/services/api';
 // import { Container } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  api.get('appointments');
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+    </>
+  );
 }
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,30 +1,85 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
 
-import { signInSuccess } from './actions';
+import { signInSuccess, signFailure } from './actions';
 
 export function* signIn({ payload }) {
-  const { email, password } = payload;
+  try {
+    const { email, password } = payload;
 
-  // call é responsável por chamar métodos assíncronos e que retornam promisses:
-  const response = yield call(api.post, 'sessions', {
-    email,
-    password,
-  });
+    // call é responsável por chamar métodos assíncronos e que retornam promisses:
+    const response = yield call(api.post, 'sessions', {
+      email,
+      password,
+    });
 
-  const { token, user } = response.data;
+    const { token, user } = response.data;
 
-  if (!user.provider) {
-    console.tron.error('Usuário não é prestador');
-    return;
+    if (!user.provider) {
+      toast.error('Usuário não é prestador');
+      return;
+    }
+
+    /**
+     * api.defaults: seta informações que serão utilizadas em todas as
+     * requisições, no caso 'token':
+     */
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+
+    // put dispara uma action de dentro do sagas:
+    yield put(signInSuccess(token, user));
+
+    history.push('/dashboard');
+  } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
+    yield put(signFailure());
   }
-
-  // put dispara uma action de dentro do sagas:
-  yield put(signInSuccess(token, user));
-
-  history.push('/dashboard');
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload;
+
+    yield call(api.post, 'users', {
+      name,
+      email,
+      password,
+      provider: true,
+    });
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados!');
+
+    yield put(signFailure());
+  }
+}
+
+export function setToken({ payload }) {
+  // se não houver nada no payload (é o primeiro acesso na aplicação):
+  if (!payload) return;
+
+  // se houver o token:
+  const { token } = payload.auth;
+  if (token) {
+    api.defaults.headers.Authorization = `Bearer ${token}`;
+  }
+}
+
+/**
+ * No saga podemos ouvir a action persist/REHYDRATE, que é disparada pelo
+ * redux-persist e também possui um payload.
+ * O redux-persist só deixa exibir o conteúdo da tela após ter recuperado os
+ * dados de dentro da storage. Assim o token sempre vai estar presente quando
+ * o usuário estiver logado e será enviado o token de autenticação a toda chamada
+ * à API.
+ */
+
+export default all([
+  takeLatest('persist/REHYDRATE', setToken),
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp),
+]);
